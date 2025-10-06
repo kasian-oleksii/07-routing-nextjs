a0857260651fe9747e035f63ec7028926acc7378
OleksiiKasian <kasianalex1@gmail.com>
2025-10-06 21:20:56 +0300

    add initial files

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,15 +1,3 @@
-// import { fetchNotes } from '@/lib/api';
-// import NotesClient from './Notes.client';
-
-// export default async function NotesPage() {
-//   const initialPage = 1;
-//   const initialQuery = '';
-
-//   const initialData = await fetchNotes(initialPage, initialQuery);
-
-//   return <NotesClient initialData={initialData} />;
-// }
-
 import {
   QueryClient,
   HydrationBoundary,
